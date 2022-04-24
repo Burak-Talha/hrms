e420ab75afb805fcc900718107b70f3e295de4d3
SipanAkcan <67048219+sxpN13@users.noreply.github.com>
2022-04-24 22:00:32 +0300

    NEW UI (Responsive)
    Only login screen

---
+++ Front-End/fusion_ui/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:fusion_ui/pages/login.dart';
+
+void main() {
+  runApp(const ToDo());
+}
---
+++ Front-End/fusion_ui/lib/theme-style/style.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class ProjectTextStyles {
+  static TextStyle mTextStyle = const TextStyle();
+}
