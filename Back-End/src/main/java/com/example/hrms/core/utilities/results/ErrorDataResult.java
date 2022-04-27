652a2fccedfbd7a68a9cab485e6e977f23314ea5
okcu73 <alptng72@gmail.com>
2022-04-27 14:14:26 +0300

    Add new Result Classes
    Add data and result class for clear presentation on JSON

---
+++ Back-End/src/main/java/com/example/hrms/core/utilities/results/ErrorDataResult.java
@@ -0,0 +1,29 @@
+package kodlamaio.northwind.core.utilities.results;
+
+import com.example.hrms.core.utilities.results.DataResult;
+
+public class ErrorDataResult<T> extends DataResult<T> {
+
+	public ErrorDataResult(T data, String message) {
+		super(data, false ,message);
+	}
+	
+	public ErrorDataResult(T data) {
+		super(data,false);
+	}
+	
+	public ErrorDataResult(String message) {
+		super(null, false ,message);
+	}
+	
+	public ErrorDataResult() {
+		super(null, false);
+	}
+
+}
+
+
+
+
+
+
