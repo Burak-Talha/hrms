a4b833d152b9ed728bf121698a035c17ba2f8f58
SipanAkcan <67048219+sxpN13@users.noreply.github.com>
2022-05-03 13:35:43 +0300

    New UI

---
+++ Front-End/hrms_ui/lib/theme/colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class ProjectColors {
+  static Color get backgroundColor => const Color.fromARGB(255, 7, 4, 17);
+}
---
+++ Front-End/hrms_ui/lib/theme/styles.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class ProjectStyles {
+  // TextStyles
+  static TextStyle titleStyle = const TextStyle(
+    color: Colors.white,
+    letterSpacing: 4,
+    fontSize: 25,
+  );
+  static TextStyle appBarTextStyle = const TextStyle(
+    color: Colors.white,
+    fontSize: 15,
+  );
+  static TextStyle containerTitle = const TextStyle(
+    color: Colors.black,
+    fontSize: 25,
+  );
+  static TextStyle containerText = const TextStyle(
+    color: Colors.black,
+    fontSize: 15,
+  );
+  static TextStyle buttonText = const TextStyle(
+    color: Colors.white,
+    fontSize: 15,
+  );
+
+  //
+}
