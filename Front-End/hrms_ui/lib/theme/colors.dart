6882348d81686abe70ffec701abc5e358add0042
SipanAkcan <67048219+sxpN13@users.noreply.github.com>
2022-06-03 23:04:55 +0300

    New Color Add

--- Front-End/hrms_ui/lib/theme/colors.dart
+++ Front-End/hrms_ui/lib/theme/colors.dart
@@ -2,4 +2,6 @@ import 'package:flutter/material.dart';
 
 class ProjectColors {
   static Color get backgroundColor => Colors.black;
+  static Color get white => Colors.white;
+  static Color get transparent => Colors.transparent;
 }
