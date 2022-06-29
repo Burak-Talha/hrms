2549fa256fb1a3c440b59788aecb2fb6b75ec723
SipanAkcan <67048219+sxpN13@users.noreply.github.com>
2022-06-29 17:45:14 +0300

    password visibility button

--- Front-End/front_end/lib/main.dart
+++ Front-End/front_end/lib/main.dart
@@ -1,9 +1,5 @@
-// ignore_for_file: unused_import
-
 import 'package:flutter/material.dart';
-import 'package:front_end/pages/home/home_employee.dart';
 import 'package:front_end/pages/login/login_employee.dart';
-import 'package:front_end/pages/profile/profile_employee.dart';
 
 import 'package:front_end/style/style.dart';
 
