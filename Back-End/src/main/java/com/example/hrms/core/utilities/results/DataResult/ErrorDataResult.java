8b44de3cb62c4f832face35e5089d074b9f4fbb9
okcu73 <alptng72@gmail.com>
2022-07-26 11:46:47 +0300

    Add new DataResult package

---
+++ Back-End/src/main/java/com/example/hrms/core/utilities/results/DataResult/ErrorDataResult.java
@@ -0,0 +1,16 @@
+package com.example.hrms.core.utilities.results.DataResult;
+
+import com.example.hrms.core.utilities.results.DataResult.DataResult;
+
+public class ErrorDataResult extends DataResult {
+	public ErrorDataResult(String message) {
+		super(false, message);
+	}
+
+}
+
+
+
+
+
+
---
+++ Back-End/src/main/java/com/example/hrms/core/utilities/results/DataResult/SuccessDataResult.java
@@ -0,0 +1,25 @@
+package com.example.hrms.core.utilities.results.DataResult;
+
+import com.example.hrms.core.utilities.results.DataResult.DataResult;
+
+import java.util.List;
+
+public class SuccessDataResult<T> extends DataResult {
+
+	public SuccessDataResult(List<T> dataList) {
+		super(dataList,true);
+	}
+
+	public SuccessDataResult(List<T> dataList, String message) {
+		super(dataList,true, message);
+	}
+
+	public SuccessDataResult(T dataList) {
+		super(dataList,true);
+	}
+
+	public SuccessDataResult(T dataList, String message) {
+		super(dataList,true, message);
+	}
+
+}
