f73aa7fc4d9abb56bd51f8ab14d3a0ee5bada601
okcu73 <alptng72@gmail.com>
2022-07-26 11:45:23 +0300

    Delete & update unnecessary classes

--- Back-End/src/main/java/com/example/hrms/entities/concretes/converters/abstracts/sub/EmployeeConverter.java
+++ Back-End/src/main/java/com/example/hrms/entities/concretes/converters/abstracts/sub/EmployeeConverter.java
@@ -3,6 +3,6 @@ package com.example.hrms.entities.concretes.converters.abstracts.sub;
 import com.example.hrms.entities.concretes.converters.abstracts.base.Converter;
 import com.example.hrms.entities.concretes.dtos.concretes.EmployerDto;
 
-public interface IEmployeeConverter extends Converter<EmployerDto> {
+public interface EmployeeConverter extends Converter<EmployerDto> {
 
 }
